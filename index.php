38ccec98e38b7ef1a33642bc68254038fa392d24
Saleema Ali <saleemaali@Saleemas-MacBook-Pro.local>
2016-09-28 20:20:22 -0400

    re-uploading Heroku files

--- index.php
+++ index.php
@@ -1,1 +1,2 @@
-<?php include_once("filename of your html file with the extension"); ?>
+<!-- Just this one line. Notice how the HTML file is passed as the only argument for the function -->
+<?php include_once("HTML-Bio-withCSS.html"); ?>
