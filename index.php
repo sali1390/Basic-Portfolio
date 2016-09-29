42cddfad99554bd50cfe5c2a383b804a7ea2e818
Saleema Ali <saleemaali@Saleemas-MacBook-Pro.local>
2016-09-28 20:39:38 -0400

    Edited index.php file

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <!-- Just this one line. Notice how the HTML file is passed as the only argument for the function -->
-<?php include_once("HTML-Bio-withCSS.html"); ?>
+<?php include_once("index.html"); ?>
